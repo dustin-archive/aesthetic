009b35bef5ca81cde6df4f68011d1140dc6212ab
Dustin Dowell <dustindowell22@gmail.com>
2016-11-22 20:02:05 -0600

    Save a byte ride a cowboy

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports=function(v){var i=v.length,r='',x;while(i--){x=v[i];r=r?x==0?r:x+' '+r:x+r};return r}
+module.exports=function(v){var i=v.length,r='',x;while(i--){x=v[i];r=r?x==0?r:x+' '+r:x+r}return r}
