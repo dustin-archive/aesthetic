727ca0ddc3480817fa4212022b9ad8590cbb1077
Dustin Dowell <dustindowell22@gmail.com>
2016-11-22 21:12:46 -0600

    Add regex to bench for comparison

--- bench.js
+++ bench.js
@@ -4,7 +4,11 @@ var Suite = require('benchmark').Benchmark.Suite
 var bench = new Suite('aesthetic')
 
 bench.add('aesthetic "world"', function () {
-  aesthetic('world')
+  aesthetic('hello        world')
+})
+
+bench.add('regex "world"', function () {
+  'hello        world'.replace(/\s+/g, ' ').split('').join(' ')
 })
 
 // Setup logger
