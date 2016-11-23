db253243214efadb86d2bc43205dd5aea95cc73a
Dustin Dowell <dustindowell22@gmail.com>
2016-11-22 21:59:39 -0600

    Bench types

--- bench.js
+++ bench.js
@@ -3,15 +3,31 @@ var aesthetic = require('./')
 var Suite = require('benchmark').Benchmark.Suite
 var bench = new Suite('aesthetic')
 
-bench.add('mac:   aesthetic "world"', function () {
-  aesthetic.mac('hello        world')
+bench.add('and:   aesthetic         ', function () {
+  aesthetic.and('hello')
 })
 
-bench.add('linux: aesthetic "world"', function () {
-  aesthetic.linux('hello        world')
+bench.add('and:   aesthetic collapse', function () {
+  aesthetic.and('hello        world')
 })
 
-// bench.add('regex "world"', function () {
+bench.add('plus:  aesthetic         ', function () {
+  aesthetic.plus('hello')
+})
+
+bench.add('plus:  aesthetic collapse', function () {
+  aesthetic.plus('hello        world')
+})
+
+bench.add('equal: aesthetic         ', function () {
+  aesthetic.equal('hello')
+})
+
+bench.add('equal: aesthetic collapse', function () {
+  aesthetic.equal('hello        world')
+})
+
+// bench.add('regex', function () {
 //   'hello        world'.replace(/\s+/g, ' ').split('').join(' ')
 // })
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-exports.mac=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x==' '&&l==' '?r:x+' '+r:x+r;l=x}return r}
+exports.and=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
 
-exports.linux=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x+l==' '?r:x+' '+r:x+r;l=x}return r}
+exports.plus=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x+l==='  '?r:x+' '+r:x+r;l=x}return r}
+
+exports.equal=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===l===' '?r:x+' '+r:x+r;l=x}return r}
