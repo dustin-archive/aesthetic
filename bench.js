82b4e44ee72b5d70433cb7106833270a0e7c3504
Jamen Marz <jamenmarz@gmail.com>
2016-11-22 21:09:53 -0700

    Reduce everything back to `&&` method.

--- bench.js
+++ bench.js
@@ -3,33 +3,17 @@ var aesthetic = require('./')
 var Suite = require('benchmark').Benchmark.Suite
 var bench = new Suite('aesthetic')
 
-bench.add('and:   aesthetic         ', function () {
-  aesthetic.and('hello')
+bench.add('aesthetic         ', function () {
+  aesthetic('hello')
 })
 
-bench.add('and:   aesthetic collapse', function () {
-  aesthetic.and('hello        world')
+bench.add('aesthetic collapse', function () {
+  aesthetic('hello        world')
 })
 
-bench.add('plus:  aesthetic         ', function () {
-  aesthetic.plus('hello')
+bench.add('regex             ', function () {
+  'hello        world'.replace(/\s+/g, ' ').split('').join(' ')
 })
-
-bench.add('plus:  aesthetic collapse', function () {
-  aesthetic.plus('hello        world')
-})
-
-bench.add('equal: aesthetic         ', function () {
-  aesthetic.equal('hello')
-})
-
-bench.add('equal: aesthetic collapse', function () {
-  aesthetic.equal('hello        world')
-})
-
-// bench.add('regex', function () {
-//   'hello        world'.replace(/\s+/g, ' ').split('').join(' ')
-// })
 
 // Setup logger
 bench.on('cycle', function(event) {
--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-exports.and=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
-
-exports.plus=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x+l==='  '?r:x+' '+r:x+r;l=x}return r}
-
-exports.equal=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===l===' '?r:x+' '+r:x+r;l=x}return r}
+module.exports=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
