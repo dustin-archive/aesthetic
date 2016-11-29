e6471113913bef7a7489c2ef81b787d2aa0e9976
Brandon Semilla <grandonbroseph+gh@gmail.com>
2016-11-29 11:54:19 -0800

    Switch to `for` loop, with benchmarks

--- bench.js
+++ bench.js
@@ -3,19 +3,28 @@ var aesthetic = require('./')
 var Suite = require('benchmark').Benchmark.Suite
 var bench = new Suite('aesthetic')
 
-bench.add('aesthetic         ', function () {
+bench.add('for           ', function () {
   aesthetic('hello')
 })
 
-bench.add('aesthetic collapse', function () {
+bench.add('for collapse  ', function () {
   aesthetic('hello        world')
 })
 
-bench.add('regex             ', function () {
+function aesthetic(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
+bench.add('while         ', function () {
+  aesthetic('hello')
+})
+
+bench.add('while collapse', function () {
+  aesthetic('hello        world')
+})
+
+bench.add('regex         ', function () {
   'hello'.replace(/\s+/g, ' ').split('').join(' ')
 })
 
-bench.add('regex collapse    ', function () {
+bench.add('regex collapse', function () {
   'hello        world'.replace(/\s+/g, ' ').split('').join(' ')
 })
 
