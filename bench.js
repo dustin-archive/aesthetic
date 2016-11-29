a7516bb8a22af86435bed4ac7f4576bff7437b35
Dustin Dowell <dustindowell22@gmail.com>
2016-11-29 15:02:09 -0600

    revert back to while method

--- bench.js
+++ bench.js
@@ -1,23 +1,14 @@
-var aesthetic_f = require('./').f
-var aesthetic_w = require('./').w
+var aesthetic= require('./')
 
 var Suite = require('benchmark').Benchmark.Suite
 var bench = new Suite('aesthetic')
 
-bench.add('for           ', function () {
-  aesthetic_f('hello')
-})
-
-bench.add('for collapse  ', function () {
-  aesthetic_f('hello        world')
-})
-
 bench.add('while         ', function () {
-  aesthetic_w('hello')
+  aesthetic('hello')
 })
 
 bench.add('while collapse', function () {
-  aesthetic_w('hello        world')
+  aesthetic('hello        world')
 })
 
 bench.add('regex         ', function () {
--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-exports.f=function(v){for(var i=v.length,r='',x,l;x=v[--i];){r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
-exports.w=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
+module.exports=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
