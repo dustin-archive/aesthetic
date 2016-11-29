c88590c2677282d41fb9ac4d5426c0a1946e3b69
Dustin Dowell <dustindowell22@gmail.com>
2016-11-29 16:10:23 -0600

    save a byte

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports=function(v){var i=v.length,r='',x,l;while(i--){x=v[i];r=r?x===' '&&l===' '?r:x+' '+r:x+r;l=x}return r}
+module.exports=function(v){var i=v.length,r='',x,l;while(i--)x=v[i],r=r?x===' '&&l===' '?r:x+' '+r:x+r,l=x;return r}
